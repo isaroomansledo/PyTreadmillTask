fa5138982452513b13e7ee1008e16c7e07e72e90
mostafa.safaie <mostafa.safaie@gmail.com>
2021-02-26 17:32:57 +0000

    add inverted to outputs

--- devices/_PMW3360DM.py
+++ devices/_PMW3360DM.py
@@ -19,24 +19,25 @@ class PMW3360DM():
                      'bits': 8, 'firstbit': machine.SPI.MSB}
         if '1' in SPI_type:
             self.SPI = machine.SPI(id=0, **SPIparams)
-            self.select = _h.Digital_output(pin='W7')
+            self.select = _h.Digital_output(pin='W7', inverted=True)
 
         elif '2' in SPI_type:
             self.SPI = machine.SPI(id=1, **SPIparams)
-            self.select = _h.Digital_output(pin='W45')
+            self.select = _h.Digital_output(pin='W45', inverted=True)
 
         elif 'soft' in SPI_type.lower():
             self.SPI = machine.SoftSPI(baudrate=500000, polarity=1, phase=0, bits=8, firstbit=machine.SPI.MSB,
                                        sck=machine.Pin(id=SCK, mode=machine.Pin.OUT, pull=machine.Pin.PULL_DOWN),
                                        mosi=machine.Pin(id=MO, mode=machine.Pin.OUT, pull=machine.Pin.PULL_DOWN),
                                        miso=machine.Pin(id=MI, mode=machine.Pin.IN))
-            self.select = _h.Digital_output(pin=CS)
+            self.select = _h.Digital_output(pin=CS, inverted=True)
 
         self.motion = _h.Digital_input(pin=MT, falling_event=eventName)
-        self.reset = _h.Digital_output(pin=reset)
+        self.reset = _h.Digital_output(pin=reset, inverted=True)
 
     def read_x(self):
-        self.
+        self.select.on()
+        
 
     def read_y(self):
         pass
