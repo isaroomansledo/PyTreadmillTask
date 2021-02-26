4da7c84fe3a5a6415a4807743eacc799bca1e924
mostafa.safaie <mostafa.safaie@gmail.com>
2021-02-26 17:11:10 +0000

    add motion sensor

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -8,7 +8,9 @@ board = Breakout_dseries_1_6()
 
 # Instantiate Devices.
 
-motionSensor = PMW3360DM(SPI_type='SPI2', eventName='motion', reset: str, MT: str, CS: str)
+motionSensor = PMW3360DM(SPI_type='SPI2', eventName='motion', reset='W43', MT='W24')
+
 right_poke  = Poke(board.port_2, rising_event = 'right_poke' , falling_event = 'right_poke_out')
 center_poke = Poke(board.port_3, rising_event = 'center_poke' , falling_event = 'center_poke_out')
 houselight  = _h.Digital_output(board.port_4.POW_A)
+machine.Pin(id=SCK, mode=machine.Pin.OUT, pul=machine.Pin.PULL_DOWN)
