69f227db0859f5179402c14f3eaaf39af401ede0
mostafa.safaie <mostafa.safaie@gmail.com>
2021-02-26 17:03:25 +0000

    0

--- devices/_PMW3360DM.py
+++ devices/_PMW3360DM.py
@@ -1,5 +1,3 @@
-from re import MULTILINE
-from typing import Match
 import pyb, machine
 import pyControl.hardware as _h
 
@@ -9,21 +7,26 @@ class PMW3360DM():
     def __init__(self,
                  SPI_type: str,
                  eventName: str,
-                 reset: _h.DigitalOutput,
-                 MT: _h.DigitalInput,
-                 MI=None: _h.DigitalInput,
-                 MO=None: _h.DigitalOutput,
-                 CS=None: _h.DigitalOutput,
-                 SCK=None: _h.DigitalOutput):
+                 reset: str,
+                 MT: str,
+                 CS: str,
+                 MI: str = None,
+                 MO: str = None,
+                 SCK: str = None):
 
-        # SPI_type = 'SPI0' or 'SPI1' or 'softSPI'
-        if '0' in SPI_type:
-            self.SPI = machine.SPI(id=0)
-        elif '1' in SPI_type:
-            self.SPI = machine.SPI(id=1)
+        # SPI_type = 'SPI1' or 'SPI2' or 'softSPI'
+        SPIparams = {'baudrate': 1000_000, 'polarity': 0, 'phase': 0,
+                     'bits': 8, 'firstbit': machine.SPI.MSB}
+        if '1' in SPI_type:
+            self.SPI = machine.SPI(id=0, **SPIparams)
+        elif '2' in SPI_type:
+            self.SPI = machine.SPI(id=1, **SPIparams)
         elif 'soft' in SPI_type.lower():
-            self.SPI = machine.SoftSPI(baudrate= 500000, sck= SCK, mosi= MO, miso= MI)
-        
-        self.motion = MT
-        self.reset = reset
-        self.select = CS
+            self.SPI = machine.SoftSPI(baudrate=500000,
+                                       sck=machine.Pin(id=SCK, mode=machine.Pin.OUT, pull=machine.Pin.PULL_DOWN),
+                                       mosi=machine.Pin(id=MO, mode=machine.Pin.OUT, pull=machine.Pin.PULL_DOWN),
+                                       miso=machine.Pin(id=MI, mode=machine.Pin.IN))
+
+        self.motion = _h.Digital_input(pin=MT, falling_event=eventName)
+        self.reset = _h.Digital_output(pin=reset)
+        self.select = _h.Digital_output(pin=CS)
