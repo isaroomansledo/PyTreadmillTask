700c0276714bdcd458e75878afbb237cb7d84a68
mostafa.safaie <mostafa.safaie@gmail.com>
2021-02-26 17:09:28 +0000

    add chip select

--- devices/_PMW3360DM.py
+++ devices/_PMW3360DM.py
@@ -19,14 +19,19 @@ class PMW3360DM():
                      'bits': 8, 'firstbit': machine.SPI.MSB}
         if '1' in SPI_type:
             self.SPI = machine.SPI(id=0, **SPIparams)
+            self.select = _h.Digital_output(pin='W7')
+
         elif '2' in SPI_type:
             self.SPI = machine.SPI(id=1, **SPIparams)
+            self.select = _h.Digital_output(pin='W45')
+
         elif 'soft' in SPI_type.lower():
             self.SPI = machine.SoftSPI(baudrate=500000,
                                        sck=machine.Pin(id=SCK, mode=machine.Pin.OUT, pull=machine.Pin.PULL_DOWN),
                                        mosi=machine.Pin(id=MO, mode=machine.Pin.OUT, pull=machine.Pin.PULL_DOWN),
                                        miso=machine.Pin(id=MI, mode=machine.Pin.IN))
+            self.select = _h.Digital_output(pin=CS)
 
         self.motion = _h.Digital_input(pin=MT, falling_event=eventName)
         self.reset = _h.Digital_output(pin=reset)
-        self.select = _h.Digital_output(pin=CS)
+        
