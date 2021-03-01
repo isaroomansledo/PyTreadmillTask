7ccb560b2e0afad35967722e583da6dcddff08ad
mostafa.safaie <mostafa.safaie@gmail.com>
2021-03-01 11:46:11 +0000

    rename odour delivery

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -9,11 +9,11 @@ board = Breakout_dseries_1_6()
 
 motionSensor = PMW3360DM(SPI_type='SPI2', eventName='motion', reset='W43', MT='W24')
 
-odour = ParallelOdourRelease(5, 2,
-                             board.port_4.DIO_A, board.port_4.DIO_B,    # Dir1
-                             board.port_4.DIO_C, board.port_4.POW_A,    # Dir2
-                             board.port_4.POW_B, board.port_5.DIO_A,    # Dir3
-                             board.port_5.DIO_B, board.port_5.POW_A,    # Dir4
-                             board.port_3.POW_A, board.port_3.POW_B)    # Dir5
+odourDelivery = ParallelOdourRelease(5, 2,
+                                     board.port_4.DIO_A, board.port_4.DIO_B,    # Dir1
+                                     board.port_4.DIO_C, board.port_4.POW_A,    # Dir2
+                                     board.port_4.POW_B, board.port_5.DIO_A,    # Dir3
+                                     board.port_5.DIO_B, board.port_5.POW_A,    # Dir4
+                                     board.port_3.POW_A, board.port_3.POW_B)    # Dir5
 
 rewardSol = Digital_output(pin=board.port_5.POW_B)
