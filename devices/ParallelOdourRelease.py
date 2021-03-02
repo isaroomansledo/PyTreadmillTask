6ff492a4229ad4f3624c5f9df27b94856903cabf
mostafa.safaie <mostafa.safaie@gmail.com>
2021-03-02 12:32:29 +0000

    EVERYTHING0-indexed

--- devices/ParallelOdourRelease.py
+++ devices/ParallelOdourRelease.py
@@ -20,7 +20,7 @@ class ParallelOdourRelease():
         for dir in range(Ndirections):
             for stim in range(NstimPerDir):
                 tmp = _h.Digital_output(pin=pins[counter])
-                self.__dict__[self._sol_name(dir + 1, stim + 1)] = tmp
+                self.__dict__[self._sol_name(dir, stim)] = tmp
                 tmp.off()
 
                 counter += 1
@@ -28,11 +28,14 @@ class ParallelOdourRelease():
     def all_off(self):
         for dir in range(self.Ndirections):
             for stim in range(self.NstimPerDir):
-                self.__dict__[self._sol_name(dir + 1, stim + 1)].off()
+                self.__dict__[self._sol_name(dir, stim)].off()
 
     def clean_air_on(self):
         for dir in range(self.Ndirections):
-            self.__dict__[self._sol_name(dir + 1, 1)].on()
+            self.__dict__[self._sol_name(dir, 0)].on()
+
+    def odour_release(self, dir: int):
+
 
     @staticmethod
     def _sol_name(dir: int, odour: int) -> str:
