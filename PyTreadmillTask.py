aaf514efd352c1cbc124bf6856a2c5049aa42dc9
AtMostafa <mostafa.safaie@gmail.com>
2021-03-17 10:44:09 +0000

    del extra code

--- PyTreadmillTask.py
+++ PyTreadmillTask.py
@@ -12,17 +12,10 @@ import init_odour
 # -------------------------------------------------------------------------
 
 states = ['intertrial',
-          'trial_start',
-          'odour_release',
-          'reward',
-          'penalty']
+          'trial_start']
 
 events = ['motion',
-          'lick',
           'session_timer',
-          'IT_duration_elapsed',
-          'odour_duration_elapsed',
-          'odour_release_delay_elapsed'
           ]
 
 initial_state = 'intertrial'
@@ -34,24 +27,6 @@ initial_state = 'intertrial'
 
 # session params
 v.session_duration = 5 * second  # 1 * hour
-v.reward_duration = 100 * ms  
-v.trial_number = 0
-v.delta_x = uarray.array('i')  # signed int minimm 2 bytes
-v.delta_y = uarray.array('i')
-
-# intertrial params
-v.min_IT_movement = 10  # cm
-v.min_IT_duration = 1 * second
-v.IT_duration_done___ = False
-
-# trial params
-v.odour_release_delay = 1  # second
-v.odour_delay_done___ = False
-v.max_odour_time = 10 * second
-v.max_odour_movement = 50  # cm
-v.distance_to_target = 20  # cm
-v.target_angle_tolerance = math.pi / 18  # deg_rad
-v.odourant_direction = -1
 
 # -------------------------------------------------------------------------
 # Define behaviour.
@@ -97,52 +72,6 @@ def trial_start(event):
         # TODO: implement the criteria
         odourDelivery.clean_air_on()
 
-
-def odour_release(event):
-    if event == 'entry':
-        odourDelivery.all_off()
-        v.odour_delay_done___ = False
-        set_timer('odour_release_delay_elapsed', v.odour_release_delay)
-        set_timer('odour_duration_elapsed', v.max_odour_time)
-    elif event == 'odour_release_delay_elapsed':  # release the odour
-        v.odour_delay_done___ = True
-        v.odourant_direction = init_odour.release_single_odourant_random(odourDelivery)
-        v.delta_x, v.delta_y = uarray.array('i'), uarray.array('i')
-    elif event == 'motion':
-        if v.odour_delay_done___:
-            arrived = init_odour.arrived_to_target(sum(v.delta_x), sum(v.delta_y),
-                                                   v.odourant_direction,
-                                                   v.distance_to_target,
-                                                   v.target_angle_tolerance)
-            if arrived is None:
-                pass
-            elif arrived is True:
-                goto_state('reward')
-            elif arrived is False:
-                goto_state('penalty')
-    elif event == 'odour_duration_elapsed':
-        goto_state('penalty')
-
-
-def reward(event):
-    if event == 'entry':
-        disarm_timer('odour_duration_elapsed')
-        set_timer('reward_duration', v.reward_duration, False)
-        rewardSol.on()
-    elif event == 'reward_duration':
-        rewardSol.off()
-        disarm_timer('reward_duration')
-        goto_state('intertrial')
-
-
-def penalty(event):
-    if event == 'entry':
-        disarm_timer('odour_duration_elapsed')
-        set_timer('penalty_duration', v.reward_duration, False)
-        # TODO: implement the penalty
-    elif event == 'penalty_duration':
-        disarm_timer('penalty_duration')
-        goto_state('intertrial')
 
 
 # State independent behaviour.
