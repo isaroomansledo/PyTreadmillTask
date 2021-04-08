d9dda7a9f6cfee2dcc18a4418517200b7e8ee8ea
mostafa.safaie <mostafa.safaie@gmail.com>
2021-04-08 12:01:11 +0100

    0

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -1,5 +1,4 @@
 # This is an example hardware definition for use with pyControl D-series breakout v1.6
-# Nosepokes are plugged into ports 1-3, and a houselight is in port 4. 
 
 from devices import *
 
