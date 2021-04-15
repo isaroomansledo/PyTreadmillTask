2ba6d1fcb540d5337c5cc7e601380cd8081d9045
mostafa.safaie <mostafa.safaie@gmail.com>
2021-04-15 14:59:24 +0100

    some cleanup

--- tasks/PyTreadmillTask.py
+++ tasks/PyTreadmillTask.py
@@ -11,11 +11,10 @@ import uarray
 
 states = ['intertrial',
           'trial_start']
+events = ['session_timer']
 
 initial_state = 'intertrial'
-events = [
-          'session_timer'
-          ]
+
 
 # -------------------------------------------------------------------------
 # Variables.
@@ -30,29 +29,29 @@ v.rand_angle = uarray.array('d')
 
 # Run start and stop behaviour.
 
-# State behaviour functions.
+
 def trial_start(event):
     if event == 'entry':
-        print('{}, timeBeforeATan'.format(get_current_time()))
+        print('timeBeforeATan')
         for angle in v.rand_angle:
-            a = math.atan2(angle, 1)
+            math.atan2(angle, 1)
         
-        print('{}, timeAfterATan'.format(get_current_time()))
+        print('timeAfterATan')
 
         stop_framework()
 
 
-
 def intertrial(event):
     if event == 'entry':
-        set_timer('session_timer', 10, True)    
-        print('{}, timeBeforeRand'.format(get_current_time()))
-        for i in range (1000):
-            v.rand_angle.append(random()*6.28)
+        set_timer('session_timer', 10, True)
+        print('timeBeforeRand')
+        for i in range(1000):
+            v.rand_angle.append(random() * 6.28)
 
-        print('{}, timeAfterRand'.format(get_current_time()))
+        print('timeAfterRand')
         goto_state('trial_start')
-        
+
+
 def all_states(event):
     if event == 'session_timer':
         stop_framework()
