c8063b06cb8292a4c6fba3e0905408e697775f1f
mostafa.safaie <mostafa.safaie@gmail.com>
2021-05-04 16:31:19 +0100

    back to simple 1ms timer

--- tasks/PyTreadmillTask.py
+++ tasks/PyTreadmillTask.py
@@ -10,13 +10,10 @@ import uarray
 # States and events.
 # -------------------------------------------------------------------------
 
-states = ['intertrial',
-          'trial_start']
+states = ['intertrial']
 
 events = ['motion',
-          'session_timer',
-          'IT_duration_elapsed'
-          ]
+          'session_timer']
 
 initial_state = 'intertrial'
 
@@ -24,25 +21,17 @@ initial_state = 'intertrial'
 # Variables.
 # -------------------------------------------------------------------------
 
-
 # session params
-v.session_duration = 30 * second  # 1 * hour
-v.min_IT_duration = 1 * second
-v.IT_duration_done___ = False
-v.delta_x = uarray.array('i')  # signed int minimm 2 bytes
-v.delta_y = uarray.array('i')
-# -------------------------------------------------------------------------
-# Define behaviour.
-# -------------------------------------------------------------------------
+v.session_duration = 1 * hour
+v.motion_timer___ = 1 * ms  # polls motion every 1ms
 
 
 # Run start and stop behaviour.
 def run_start():
     # Code here is executed when the framework starts running.
     set_timer('session_timer', v.session_duration, True)
-
     hw.motionSensor.power_up()
-    hw.motionSensor.select.on()
+    set_timer('motion', v.motion_timer___)
 
 
 def run_end():
@@ -54,25 +43,8 @@ def run_end():
 
 # State behaviour functions.
 def intertrial(event):
-    if event == 'entry':
-        set_timer('IT_duration_elapsed', v.min_IT_duration)
-        hw.led.on()
-    elif event == 'IT_duration_elapsed':
-        v.IT_duration_done___ = True
-    elif event == 'motion':
-        if v.IT_duration_done___:
-            hw.led.off()
-            v.IT_duration_done___ = False
-            goto_state('trial_start')
-
-
-def trial_start(event):
-    if event == 'entry':
-        disarm_timer('IT_duration_elapsed')
-        hw.led2.on()
-    elif event == 'motion':
-        hw.led2.off()
-        goto_state('intertrial')
+    if event == 'motion':
+        print('mo')
 
 
 # State independent behaviour.
@@ -82,11 +54,8 @@ def all_states(event):
     if event == 'motion':
         # read the motion registers and and append the variables
         delta_x, delta_y = hw.motionSensor.read_pos()
-        v.delta_x.append(delta_x)
-        v.delta_y.append(delta_y)
-
-        print('{},{}'.format(v.delta_x[-1], v.delta_y[-1]))
-        hw.motionSensor.select.on()
+        print('{},{}, dM'.format(delta_x, delta_y))
+        set_timer('motion', v.motion_timer___)
 
     elif event == 'session_timer':
         stop_framework()
