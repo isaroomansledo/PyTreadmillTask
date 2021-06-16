5f685347be208522574fd6299301efa5b2a0f607
mostafa.safaie <mostafa.safaie@gmail.com>
2021-06-16 13:45:07 +0100

    less fs

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -11,4 +11,4 @@ board = Breakout_dseries_1_6()
 # motionSensor = PMW3360DM(SPI_type='SPI2', eventName='',
 #                          reset=board.port_3.DIO_B, MT=board.port_3.DIO_C)
 
-motionSensor = MotionDetector(name='test', sampling_rate=5000, reset=board.port_3.DIO_B, event='motion')
+motionSensor = MotionDetector(name='test', sampling_rate=500, reset=board.port_3.DIO_B, event='motion')
